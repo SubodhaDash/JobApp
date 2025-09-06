4d5f8b6b9d111a763645345ae88abe5414d98a53
SubodhaDash <subodhadash64@gmail.com>
2025-09-06 12:02:25 +0530

    fix(controller): add methods for allJobs, addjobs,etc. feat(model): introduce JobPost model. feat(repo): introduce Jobrepo layer. feat(service): introduce JobService layer. fix(properties): update application.properties.

--- src/main/java/com/subodha/JobApp/service/JobService.java
+++ src/main/java/com/subodha/JobApp/service/JobService.java
@@ -1,4 +1,25 @@
 package com.subodha.JobApp.service;
 
+import com.subodha.JobApp.model.JobPost;
+import com.subodha.JobApp.repo.JobRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+
+
+@Service
 public class JobService {
+
+    @Autowired
+    private JobRepo repo;
+
+    public void addJob(JobPost jobPost){
+        repo.addJob(jobPost);
+    }
+
+    public List<JobPost> getAllJobs(){
+        return repo.getAllJobs();
+    }
 }
